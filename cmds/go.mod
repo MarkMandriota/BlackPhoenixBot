2be55fff6758139d7c23bb123beea235bde86a7e
Nameless Name <62650188+MarkMandriota@users.noreply.github.com>
2021-02-09 10:51:34 +0100

    Add files via upload

---
+++ cmds/go.mod
@@ -0,0 +1,3 @@
+module github.com/MarkMandriota/BlackPhoenixBot/cmds
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	dg "github.com/bwmarrin/discordgo"
+)
+
+var (
+	config = make(map[string]string)
+)
+
+func init() {
+	arr, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		log.Fatalf("Error while reading configuration file")
+	}
+
+	if err := json.Unmarshal(arr, &config); err != nil {
+		log.Fatalf("Incorrect configuration file format")
+	}
+}
+
+func main() {
+	s, err := dg.New(config["token"])
+	if err != nil {
+		log.Fatalf("Error creating Discord session")
+	}
+
+	s.AddHandler(messageCreate)
+
+	s.Identify.Intents = dg.IntentsGuildMessages
+
+	if err := s.Open(); err != nil {
+		log.Fatalf("Error openning connection")
+	}
+	defer s.Close()
+
+	log.Printf("Bot is running...")
+
+	wait := make(chan os.Signal)
+	signal.Notify(wait, syscall.SIGINT)
+	<-wait
+}
+
+func messageCreate(s *dg.Session, m *dg.MessageCreate) {
+	defer func() {
+		if err := recover(); err != nil {
+			switch err.(type) {
+			case *dg.MessageEmbed:
+				s.ChannelMessageSendEmbed(m.ChannelID, err.(*dg.MessageEmbed))
+			default:
+				log.Printf("%v", err)
+			}
+		}
+	}()
+
+	if !strings.HasPrefix(m.Content, config["prefix"]) || m.Author.Bot {
+		return
+	}
+}
