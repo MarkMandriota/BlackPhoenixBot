954ef8dfc365bd943d64c7ef5941df616ec4acac
Nameless Name <62650188+MarkMandriota@users.noreply.github.com>
2021-04-22 17:58:25 +0200

    Add files via upload

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"reflect"
+)
+
+var config = struct {
+	Token  string `env:"TOKEN"`
+	Prefix string `env:"PREFIX"`
+}{}
+
+func init() {
+	confV := reflect.ValueOf(&config).Elem()
+	confT := confV.Type()
+
+	for i := 0; i < confV.NumField(); i++ {
+		fieldV := confV.Field(i)
+		fieldT := confT.Field(i)
+
+		if fieldT.Type.Kind() == reflect.String && fieldV.CanSet() {
+			fieldV.SetString(os.Getenv(fieldT.Tag.Get("env")))
+		}
+	}
+}
+
+func main() {
+	fmt.Println(reflect.ValueOf(math.Sqrt).Call([]reflect.Value{
+		reflect.ValueOf(2.),
+	})[0].Interface())
+}
