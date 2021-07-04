6ba2c10b16ba29f742744ccb6a90b962c7832517
Nameless Name <62650188+MarkMandriota@users.noreply.github.com>
2021-07-04 21:55:22 +0200

    Add files via upload

--- parser/token_test.go
+++ parser/token_test.go
@@ -1,3 +1,8 @@
+// Copyright 2021 Mark Mandriota. All right reserved.
+// Use of this source code is governed by a Apache 2.0-style
+// license that can be found in the LICENSE file.
+
+
 package parser
 
 import (
